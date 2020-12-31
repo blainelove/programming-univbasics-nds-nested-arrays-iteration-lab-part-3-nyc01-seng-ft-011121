68f6f8b0696f12e480a0903c7a9f88aba2c648c5
Blaine R Love <blainelove94@gmail.com>
2020-12-31 21:35:53 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,10 +1,10 @@
 def join_nested_strings(src)
   row_index = 0
-  string_array[]
+  string_array = []
   while row_index < src.count do
     element_index = 0
     while element_index < src[row_index].count do
-      if src[row_index][element_index].is_string? == true 
+      if src[row_index][element_index].is_string? == true
         string_array << src[row_index][element_index]
       end
       element_index += 1
